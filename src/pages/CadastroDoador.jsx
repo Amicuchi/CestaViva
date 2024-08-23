8e9582fa1073cc75c11c3dc5aea43ec695772b85
AndersonAmicuchi <AndersonAmicuchi@gmail.com>
2024-08-23 20:31:24 -0300

    Alteração no estilo do Cadastro de Doador

--- src/pages/CadastroDoador.jsx
+++ src/pages/CadastroDoador.jsx
@@ -1,9 +1,11 @@
+import '../styles/CadastroDoador.css'
 import { useState } from 'react';
 
 function CadastroDoador() {
     const [nome, setNome] = useState('');
     const [email, setEmail] = useState('');
     const [senha, setSenha] = useState('');
+    const [senha2, setSenha2] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -13,12 +15,13 @@ function CadastroDoador() {
 
     return (
         <div>
-            <h2>Cadastro de Doador</h2>
-            <form onSubmit={handleSubmit}>
-                <input type="text" placeholder="Nome" value={nome} onChange={(e) => setNome(e.target.value)} required />
-                <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-                <input type="password" placeholder="Senha" value={senha} onChange={(e) => setSenha(e.target.value)} required />
-                <button type="submit">Cadastrar</button>
+            <h2 className='TitleH2'>Cadastro de Doador</h2>
+            <form className='CDForm' onSubmit={handleSubmit}>
+                <input type="text" className='CDInput' id="CDNome" name="CDNome" placeholder="Nome" value={nome} onChange={(e) => setNome(e.target.value)} required />
+                <input type="email" className='CDInput' id="CDEmail" name="CDEmail" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                <input type="password" className='CDInput' id="CDSenha" name="CDSenha" placeholder="Senha" value={senha} onChange={(e) => setSenha(e.target.value)} required />
+                <input type="password" className='CDInput' id="CDConfirmaSenha" name="CDConfirmaSenha" placeholder="Confirme sua Senha" value={senha2} onChange={(e) => setSenha2(e.target.value)} required />
+                <button type="submit" className="CDButton">Cadastrar</button>
             </form>
         </div>
     );
