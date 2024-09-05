446c83e806e4799afdbbf642f12be9b16336b842
AndersonAmicuchi <AndersonAmicuchi@gmail.com>
2024-09-05 10:16:53 -0300

    Exclusão do botão de cadastro de doador

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
                 <li><Link to="/">Página Inicial</Link></li>
                 <li><Link to="/busca">Busca</Link></li>
                 <li><Link to="/cadastro-entidade">Cadastrar Entidade</Link></li>
-                <li><Link to="/cadastro-doador">Cadastrar Doador</Link></li>
+                {/* <li><Link to="/cadastro-doador">Cadastrar Doador</Link></li> */}
                 <li><Link to="/login">Entrar</Link></li>
             </ul>
         </nav>
