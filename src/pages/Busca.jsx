bb396b55136d01807b9e423c71d7d4835561e33b
AndersonAmicuchi <AndersonAmicuchi@gmail.com>
2024-09-06 19:14:40 -0300

    Alteração na estrutura e estilo da busca

--- src/pages/Busca.jsx
+++ src/pages/Busca.jsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from 'react';
 import api from '../services/api'; // Simula uma chamada à API
-import '../styles/Busca.css'
+import '../styles/Busca.css';
 
 export default function Busca() {
     const [entidades, setEntidades] = useState([]);
-    const [filtro, setFiltro] = useState('');
+    const [filtroCidade, setFiltroCidade] = useState('');
+    const [filtroItem, setFiltroItem] = useState('');
 
     useEffect(() => {
         // Simulação de uma chamada à API para buscar as entidades
@@ -13,33 +14,48 @@ export default function Busca() {
             .catch(error => console.error('Erro ao buscar entidades:', error));
     }, []);
 
-    // Filtrar entidades baseado no filtro
-    const entidadesFiltradas = filtro === '' 
-        ? entidades 
-        : entidades.filter(entidade => entidade.necessidades.includes(filtro)
-    );
+    // Filtrar entidades baseado nos filtros de cidade e item
+    const entidadesFiltradas = entidades.filter(entidade => {
+        const filtroCidadeAplicado = filtroCidade === '' || entidade.cidade.toLowerCase() === filtroCidade.toLowerCase();
+        const filtroItemAplicado = filtroItem === '' || entidade.necessidades.includes(filtroItem);
+        return filtroCidadeAplicado && filtroItemAplicado;
+    });
 
     return (
         <main className='buscaContainer'>
             <h1>Busca de Entidades</h1>
-            <select onChange={(e) => setFiltro(e.target.value)}>
-                <option value="">Todos os alimentos</option>
-                <option value="arroz">Arroz</option>
-                <option value="feijao">Feijão</option>
-                {/* Adicionar as opções conforme necessário */}
-            </select>
+            <div className='buscaSelects'>
+                <select onChange={(e) => setFiltroCidade(e.target.value)}>
+                    <option value="">Todas as cidades</option>
+                    <option value="Cravinhos">Cravinhos</option>
+                    <option value="Franca">Franca</option>
+                    <option value="Jardinópolis">Jardinópolis</option>
+                    <option value="Ribeirão Preto">Ribeirão Preto</option>
+                    <option value="Serrana">Serrana</option>
+                    <option value="Taquaritinga">Taquaritinga</option>
+                    {/* Adicionar as opções conforme necessário */}
+                </select>
+                <select onChange={(e) => setFiltroItem(e.target.value)}>
+                    <option value="">Todos os alimentos</option>
+                    <option value="arroz">Arroz</option>
+                    <option value="feijao">Feijão</option>
+                    {/* Adicionar as opções conforme necessário */}
+                </select>
+            </div>
             <ul className='buscaUL'>
                 {entidadesFiltradas.length === 0 ? (
                     <li className='BELi'>Nenhuma entidade encontrada.</li>
                 ) : (
                     entidadesFiltradas.map((entidade) => (
-                        <li className="BELi" key={entidade.id}>
-                            {entidade.nome} - {entidade.distancia} km de distância
-                            <ul>
+                        <li className="BELi card" key={entidade.id}>
+                            <h2 className="entidadeNome">{entidade.nome}</h2>
+                            <p className="entidadeEndereco">{entidade.endereco}</p>
+                            <p className="entidadeEndereco">{entidade.cidade}</p>
+                            <div className="entidadeNecessidades">
                                 {entidade.necessidades.map((necessidade, index) => (
-                                    <li key={index}>{necessidade}</li>
+                                    <span className="necessidade" key={index}>{necessidade}</span>
                                 ))}
-                            </ul>
+                            </div>
                         </li>
                     ))
                 )}
--- src/services/api.js
+++ src/services/api.js
@@ -3,18 +3,18 @@ import iczs from '../assets/iczs.jpg';
 import cristoreiribeirao from '../assets/cristoreiribeirao.jpg';
 
 const entidadesMock = [
-    { id: 1, nome: 'Igreja do Evangelho Quadrangular', distancia: 2.5, necessidades: ['arroz', 'feijao'], img: ieq },
-    { id: 2, nome: 'Igreja Cristã da Zona Sul', distancia: 5.0, necessidades: ['feijao'], img: iczs },
-    { id: 3, nome: 'Igreja Cristo Rei - Rib Preto', distancia: 1.2, necessidades: ['arroz'], img: cristoreiribeirao },
-    { id: 4, nome: 'Igreja do Evangelho Quadrangular', distancia: 2.5, necessidades: ['arroz', 'feijao'], img: ieq },
-    { id: 5, nome: 'Igreja Cristã da Zona Sul', distancia: 5.0, necessidades: ['feijao'], img: iczs },
-    { id: 6, nome: 'Igreja Cristo Rei - Rib Preto', distancia: 1.2, necessidades: ['arroz'], img: cristoreiribeirao },
-    { id: 7, nome: 'Igreja do Evangelho Quadrangular', distancia: 2.5, necessidades: ['arroz', 'feijao'], img: ieq },
-    { id: 8, nome: 'Igreja Cristã da Zona Sul', distancia: 5.0, necessidades: ['feijao'], img: iczs },
-    { id: 9, nome: 'Igreja Cristo Rei - Rib Preto', distancia: 1.2, necessidades: ['arroz'], img: cristoreiribeirao },
-    { id: 10, nome: 'Igreja do Evangelho Quadrangular', distancia: 2.5, necessidades: ['arroz', 'feijao'], img: ieq },
-    { id: 11, nome: 'Igreja Cristã da Zona Sul', distancia: 5.0, necessidades: ['feijao'], img: iczs },
-    { id: 12, nome: 'Igreja Cristo Rei - Rib Preto', distancia: 1.2, necessidades: ['arroz'], img: cristoreiribeirao },
+    { id: 1, nome: 'Igreja do Evangelho Quadrangular', endereco: 'Rua da entidade, 123 - Bairro onde ela fica', cidade: 'Ribeirão Preto', necessidades: ['arroz', 'feijao'], img: ieq },
+    { id: 2, nome: 'Igreja Cristã da Zona Sul', endereco: 'Rua da entidade, 123 - Bairro onde ela fica', cidade: 'Franca', necessidades: ['feijao'], img: iczs },
+    { id: 3, nome: 'Igreja Cristo Rei - Rib Preto', endereco: 'Rua da entidade, 123 - Bairro onde ela fica', cidade: 'Ribeirão Preto', necessidades: ['arroz'], img: cristoreiribeirao },
+    { id: 4, nome: 'Igreja do Evangelho Quadrangular', endereco: 'Rua da entidade, 123 - Bairro onde ela fica', cidade: 'Taquaritinga', necessidades: ['arroz', 'feijao'], img: ieq },
+    { id: 5, nome: 'Igreja Cristã da Zona Sul', endereco: 'Rua da entidade, 123 - Bairro onde ela fica', cidade: 'Franca', necessidades: ['feijao'], img: iczs },
+    { id: 6, nome: 'Igreja Cristo Rei - Rib Preto', endereco: 'Rua da entidade, 123 - Bairro onde ela fica', cidade: 'Serrana', necessidades: ['arroz'], img: cristoreiribeirao },
+    { id: 7, nome: 'Igreja do Evangelho Quadrangular', endereco: 'Rua da entidade, 123 - Bairro onde ela fica', cidade: 'Jardinópolis', necessidades: ['arroz', 'feijao'], img: ieq },
+    { id: 8, nome: 'Igreja Cristã da Zona Sul', endereco: 'Rua da entidade, 123 - Bairro onde ela fica', cidade: 'Ribeirão Preto', necessidades: ['feijao'], img: iczs },
+    { id: 9, nome: 'Igreja Cristo Rei - Rib Preto', endereco: 'Rua da entidade, 123 - Bairro onde ela fica', cidade: 'Cravinhos', necessidades: ['arroz'], img: cristoreiribeirao },
+    { id: 10, nome: 'Igreja do Evangelho Quadrangular', endereco: 'Rua da entidade, 123 - Bairro onde ela fica', cidade: 'Ribeirão Preto', necessidades: ['arroz', 'feijao'], img: ieq },
+    { id: 11, nome: 'Igreja Cristã da Zona Sul', endereco: 'Rua da entidade, 123 - Bairro onde ela fica', cidade: 'Ribeirão Preto', necessidades: ['feijao'], img: iczs },
+    { id: 12, nome: 'Igreja Cristo Rei - Rib Preto', endereco: 'Rua da entidade, 123 - Bairro onde ela fica', cidade: 'Ribeirão Preto', necessidades: ['arroz'], img: cristoreiribeirao },
 ];
 
 const api = {
