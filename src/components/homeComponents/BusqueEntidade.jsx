008ebd80b92053130bd20c5959b66b8c71e3bf56
Anderson Amicuchi <andersonamicuchi@gmail.com>
2024-09-07 19:10:22 -0300

    Correções menores no estilo da Home

--- src/components/homeComponents/BusqueEntidade.jsx
+++ src/components/homeComponents/BusqueEntidade.jsx
@@ -3,7 +3,9 @@ import '../../styles/Home.css';
 export default function BusqueEntidade() {
     return(
         <main className="BEContainer">
-            <form className="BEForm" action="">
+            <h1>Bem-vindo ao CestaViva</h1>
+            <p>Facilitando a doação de alimentos para quem mais precisa.</p>
+            <form action="">
                 <h2>Busque uma entidade:</h2>
                 <input className="BEInput" type="text" name="busqueEntidade" placeholder="Busque uma entidade aqui"/>
                 <button className="BEButton">Buscar</button>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,8 +7,6 @@ import SobreProjeto from "../components/homeComponents/SobreProjeto";
 export default function Home() {
     return (
         <>
-            <h1>Bem-vindo ao CestaViva</h1>
-            <p>Facilitando a doação de alimentos para quem mais precisa.</p>
             <BusqueEntidade />
             <SobreProjeto />
             <SobreNos />
