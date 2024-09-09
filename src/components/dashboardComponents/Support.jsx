e2bad020ae318fa4cccb5971919ed57bfdc9699b
AndersonAmicuchi <AndersonAmicuchi@gmail.com>
2024-09-09 08:06:19 -0300

    inclusão da classe card--container na div principal

--- src/components/dashboardComponents/Support.jsx
+++ src/components/dashboardComponents/Support.jsx
@@ -1,8 +1,8 @@
 export default function Support() {
     return(
-        <>
+        <div className="card--container">
             <h2>Suporte</h2>
             <p>Aqui, o usuário pode enviar imagens, arquivos e texto para o suporte, de maneira que possa abrir um ticket de solicitação de suporte para incluir, excluir ou alterar qualquer informação sobre a entidade.</p>
-        </>
+        </div>
     )
 }
