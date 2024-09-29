9e6461659841ab4553dd164b959ec1c24cc8ab7f
AndersonAmicuchi <AndersonAmicuchi@gmail.com>
2024-09-29 19:09:39 -0300

    finalização do formulário de contato com o suporte. Falta incluir a tabela de tickets abertos e fechados (histórico de tickets)

--- src/components/dashboardComponents/Support.jsx
+++ src/components/dashboardComponents/Support.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import axios from 'axios';
 
 export default function Support() {
@@ -35,47 +35,65 @@ export default function Support() {
             setLoading(false);
         }
     };
-
+    const fileInputRef = useRef(null);
     const handleClear = () => {
         setTipo('');
         setMensagem('');
         setArquivos([]);
         setProtocolo('');
+        if (fileInputRef.current) {
+            fileInputRef.current.value = '';
+        }
     };
 
     return (
         <div className="card--container lastOne">
+            <form onSubmit={handleSubmit} className='form--support'>
             <h2>Contato com o Suporte</h2>
-            <p>Aqui, o usuário pode enviar imagens, arquivos e texto para o suporte, de maneira que possa abrir um ticket de solicitação de suporte para incluir, excluir ou alterar qualquer informação sobre a entidade.</p>
-            <form onSubmit={handleSubmit}>
-                <label className='UserLabel'>Tipo de Solicitação:</label>
-                <select value={tipo} onChange={(e) => setTipo(e.target.value)} required>
-                    <option value="">Selecione</option>
-                    <option value="duvida">Dúvida</option>
-                    <option value="dificuldade">Dificuldade</option>
-                    <option value="sugestao">Sugestão</option>
-                    <option value="comentario">Comentário</option>
-                    <option value="alteracao_entidade">Alteração de dados da Entidade</option>
-                    <option value="alteracao_responsavel">Alteração de dados do Responsável</option>
-                    <option value="alteracao_usuario">Alteração de dados do Voluntário</option>
-                </select>
+            <p>Aqui você pode enviar imagens, arquivos e texto para o suporte, de maneira que possa abrir fazer uma solicitação para incluir, excluir ou alterar qualquer informação sobre a entidade.</p>
+            <hr />
+                <span className='form--span'>
+                    <label className='UserLabel'>Tipo de Solicitação:</label>
+                    <select value={tipo} onChange={(e) => setTipo(e.target.value)} required>
+                        <option value="">Selecione</option>
+                        <option value="duvida">Dúvida</option>
+                        <option value="dificuldade">Dificuldade</option>
+                        <option value="sugestao">Sugestão</option>
+                        <option value="comentario">Comentário</option>
+                        <option value="alteracao_entidade">Alteração de dados da Entidade</option>
+                        <option value="alteracao_responsavel">Alteração de dados do Responsável</option>
+                        <option value="alteracao_usuario">Alteração de dados do Voluntário</option>
+                    </select>
+                </span>
 
-                <label className='UserLabel'>Mensagem:</label>
-                <textarea value={mensagem} onChange={(e) => setMensagem(e.target.value)} required />
+                <div>
+                    <label className='UserLabel'>Mensagem:</label>
+                    <textarea value={mensagem} onChange={(e) => setMensagem(e.target.value)} required />
+                </div>
 
-                <label className='UserLabel'>Enviar Arquivos:</label>
-                <input type="file" multiple onChange={handleFileChange} />
+                <div>
+                    <label className='UserLabel'>Enviar Arquivos:</label>
+                    <input type="file" multiple onChange={handleFileChange} ref={fileInputRef} />
+                </div>
 
                 {loading ? <p>Enviando...</p> : null}
                 {erro ? <p className='errorMessage'>{erro}</p> : null}
 
-                <button type="submit">Enviar</button>
-                <button
-                    type="button"
-                    onClick={handleClear}
-                >
-                    Limpar Formulário
-                </button>
+                <span className='form--span'>
+                    <button
+                        type="submit"
+                        disabled={loading}
+                    >
+                        Enviar
+                    </button>
+
+                    <button
+                        type="button"
+                        onClick={handleClear}
+                    >
+                        Limpar Formulário
+                    </button>
+                </span>
             </form>
 
             {protocolo && <p>Protocolo: {protocolo}</p>}
