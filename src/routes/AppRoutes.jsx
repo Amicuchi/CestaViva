e0e2e64bc06083f5e33c7156ffe0696d99ad76ec
AndersonAmicuchi <AndersonAmicuchi@gmail.com>
2024-10-03 10:04:36 -0300

    refatoração do arquivo AppRoutes.jsx

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,31 +1,32 @@
-import { Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+
+import Busca from "../pages/Busca/Busca";
+import CadastroEntidade from "../pages/CadastroEntidade/CadastroEntidade";
+import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
+
+import RedefinirSenha from "../pages/RedefinirSenha/RedefinirSenha";
+import SolicitarRedefinicaoSenha from "../pages/RedefinirSenha/SolicitarRedefinicaoSenha";
+
 import PrivateRoute from './PrivateRoute';
-
-import Home from '../pages/Home/Home'
-import Busca from '../pages/Busca/Busca';
-import CadastroEntidade from '../pages/CadastroEntidade/CadastroEntidade';
-import Login from '../pages/Login/Login';
-
 import Dashboard from '../pages/Dashboard/Dashboard';
+
 import DashboardHome from '../pages/Dashboard/views/DashboardHome/DashboardHome';
 import DashboardUser from '../pages/Dashboard/views/User';
-import Settings from '../pages/Dashboard/views/Settings/Settings';
 import Cestas from '../pages/Dashboard/views/Campanhas/Cestas';
-import FAQ from '../pages/Dashboard/views/FAQ/FAQ';
+import DashboardFAQ from '../pages/Dashboard/views/FAQ/FAQ';
+import Settings from '../pages/Dashboard/views/Settings/Settings';
 
-// import RedefinirSenha from '../../components/RedefinirSenha'
-import RedefinirSenha from '../pages/RedefinirSenha/RedefinirSenha'
-import SolicitarRedefinicaoSenha from '../pages/RedefinirSenha/SolicitarRedefinicaoSenha';
-
-const AppRoutes = () => {
+export default function AppRoutes() {
     return (
-        <>
+        <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/solicitar-redefinicao-senha" element={<SolicitarRedefinicaoSenha />} />
-            <Route path="/redefinir-senha/:token" element={<RedefinirSenha />} />
             <Route path="/busca" element={<Busca />} />
             <Route path="/cadastro-entidade" element={<CadastroEntidade />} />
+            <Route path="/login" element={<Login />} />
+
+            <Route path="/solicitar-redefinicao-senha" element={<SolicitarRedefinicaoSenha />} />
+            <Route path="/redefinir-senha/:token" element={<RedefinirSenha />} />
 
             <Route
                 path="/dashboard"
@@ -38,12 +39,10 @@ const AppRoutes = () => {
                 <Route path="dashboardhome" element={<DashboardHome />} />
                 <Route path="perfil" element={<DashboardUser />} />
                 <Route path="cestas" element={<Cestas />} />
-                <Route path="faq" element={<FAQ />} />
+                <Route path="faq" element={<DashboardFAQ />} />
                 <Route path="configuracoes" element={<Settings />} />
             </Route>
             <Route path="*" element={<div>Nenhuma página encontrada.</div>} />
-        </>
+        </Routes>
     );
 }
-
-export default AppRoutes;
