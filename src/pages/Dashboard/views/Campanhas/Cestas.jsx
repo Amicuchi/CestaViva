2618e22b21f6cda685e3f97eb929c900a0491035
AndersonAmicuchi <AndersonAmicuchi@gmail.com>
2024-10-03 10:09:27 -0300

    Exclusão de comentário obsoleto

--- src/pages/Dashboard/views/Campanhas/Cestas.jsx
+++ src/pages/Dashboard/views/Campanhas/Cestas.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import api from "../../../../services/axiosConfig";
 
-// atual
 export default function Cestas() {
     const [produtos, setProdutos] = useState(() => {
         // Carrega os dados do localStorage apenas uma vez, quando o componente é montado
