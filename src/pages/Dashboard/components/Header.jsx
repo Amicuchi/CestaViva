7bb747eca8c0cfd7750d4d0a866e562e2435272b
AndersonAmicuchi <AndersonAmicuchi@gmail.com>
2024-10-15 21:35:32 -0300

    Header: Alteração para exibir o título de maneira dinâmica e exibir o botão de NovaCampanha quando abrir o Gerenciar Campanhas

--- src/pages/Dashboard/components/Header.jsx
+++ src/pages/Dashboard/components/Header.jsx
@@ -1,11 +1,21 @@
+import PropTypes from 'prop-types';
 import '../Dashboard.css';
 
-export default function Header() {
+export default function Header({ title, abrirModal }) {
     return (
-        <>
             <div className="header--wrapper">
-                <h2>Painel de controle</h2>
+                <h2>{title}</h2>
+
+                {/* Exibe o botão apenas se o título for "Gerenciamento de Campanhas" */}
+                {title === "Gerenciamento de Campanhas" && (
+                    <button onClick={abrirModal}>Nova Campanha</button>
+                )}
             </div>
-        </>
     );
 }
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+    abrirModal: PropTypes.func,
+  };
+  
