cd660e8b48dbffdb611da7cda4befaf82f620f63
AndersonAmicuchi <AndersonAmicuchi@gmail.com>
2024-10-15 21:36:34 -0300

    Campanhas: Exclusão do título e botão com a transferência desses componentes para o Header e Dashboard, respectivamente.

--- src/pages/Dashboard/views/Campanhas/Campanhas.jsx
+++ src/pages/Dashboard/views/Campanhas/Campanhas.jsx
@@ -1,31 +1,35 @@
+//Campanhas
 import { useState, useEffect, useCallback } from 'react';
-
-import ModalCampanha from './components/ModalCampanha';
 import api from "../../../../services/axiosConfig";
-import './components/ListaCampanhas.modules.css';
+import PropTypes from "prop-types";
+import ModalCampanha from './components/ModalCampanha';
 import ListaCampanhas from './components/ListaCampanhas';
+import './components/ListaCampanhas.modules.css';
 
-export default function Campanhas() {
-    const [isModalOpen, setModalOpen] = useState(false);  // Estado para abrir e fechar o modal
-    const abrirModal = () => setModalOpen(true);          // Função para abrir o modal
-    const fecharModal = () => setModalOpen(false);        // Função para fechar o modal
-
-    const [campanhas, setCampanhas] = useState([]);       // Estado para armazenar as campanhas
+export default function Campanhas({ isModalOpen, setIsModalOpen }) {
+    // Estado para armazenar as campanhas
+    const [campanhas, setCampanhas] = useState([]);
 
     // Função para buscar as campanhas no backend
     const fetchCampanhas = useCallback(async () => {
         try {
-            const response = await api.get("/cestas");
+            const response = await api.get("/cestas"); // Faz a requisição para pegar as campanhas
             setCampanhas(response.data);  // Atualiza o estado com os dados das campanhas
         } catch (error) {
-            // setError("Erro ao buscar campanhas.");
             console.error("Erro ao tentar buscar campanhas:", error.message);
         }
     }, []);
 
+    // Efeito colateral para buscar campanhas ao montar o componente
     useEffect(() => {
+        // console.log("Buscando campanhas...");  // 
         fetchCampanhas();  // Carrega as campanhas ao montar o componente
     }, [fetchCampanhas]);
+    
+    // Log para monitorar o estado e tentar encontrar a dupla renderização
+    // useEffect(() => {
+    //     console.log("Estado de campanhas atualizado:", campanhas); // Log para monitorar o estado
+    // }, [campanhas]);
 
     // Função para salvar uma nova campanha
     const handleSave = async (novaCampanha) => {
@@ -37,28 +41,25 @@ export default function Campanhas() {
             console.error("Erro ao salvar campanha:", error);
             alert("Falha ao salvar a campanha.");
         }
-        fecharModal();
+        setIsModalOpen(false); // Fecha o modal após salvar a campanha
     };
 
     return (
         <div>
-            <div className='table-header header--wrapper'>
-                <h1>Gerenciador de Campanhas</h1>
-
-                {/* Botão para abrir o modal de nova campanha */}
-                <button onClick={abrirModal}>Nova Campanha</button>
-            </div>
-
-
             {/* Lista de campanhas com acordeon */}
             <ListaCampanhas campanhas={campanhas} />
 
             {/* Modal para cadastrar nova campanha */}
             <ModalCampanha
-                isOpen={isModalOpen}
-                onClose={fecharModal}
-                onSave={handleSave}
+                isOpen={isModalOpen} // Controla a visibilidade do modal
+                onClose={() => setIsModalOpen(false)} // Função para fechar o modal
+                onSave={handleSave} // Função que será chamada ao salvar a nova campanha
             />
         </div>
     );
-};
+}
+
+Campanhas.propTypes = {
+    isModalOpen: PropTypes.bool,
+    setIsModalOpen: PropTypes.func
+};  
