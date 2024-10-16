ca80920c7840664197cdfecbb24c9ef3c612a157
AndersonAmicuchi <AndersonAmicuchi@gmail.com>
2024-10-15 22:14:32 -0300

    App.jsx: Mera alteração visual por exclusão de espaços. Nada que altere o funcionamento do código.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { BrowserRouter as Router } from 'react-router-dom';
-
 import AppRoutes from './routes/AppRoutes';
-
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
@@ -13,4 +11,4 @@ export default function App() {
             <Footer />
         </Router>
     );
-}
+};
