ba62e302b35914197ca18eef763cbdf193539de9
AndersonAmicuchi <AndersonAmicuchi@gmail.com>
2024-11-07 23:15:31 -0300

    Header: Alteração de tag comum por tag semântica

--- src/pages/Dashboard/components/Header.jsx
+++ src/pages/Dashboard/components/Header.jsx
@@ -3,19 +3,18 @@ import '../Dashboard.css';
 
 export default function Header({ title, abrirModal }) {
     return (
-            <div className="header--wrapper">
-                <h2>{title}</h2>
+        <header className="header--wrapper">
+            <h2>{title}</h2>
 
-                {/* Exibe o botão apenas se o título for "Gerenciamento de Campanhas" */}
-                {title === "Gerenciamento de Campanhas" && (
-                    <button onClick={abrirModal}>Nova Campanha</button>
-                )}
-            </div>
+            {/* Exibe o botão apenas se o título for "Gerenciamento de Campanhas" */}
+            {title === "Gerenciamento de Campanhas" && (
+                <button onClick={abrirModal}>Nova Campanha</button>
+            )}
+        </header>
     );
 }
 
 Header.propTypes = {
     title: PropTypes.string.isRequired,
     abrirModal: PropTypes.func,
-  };
-  
+};
