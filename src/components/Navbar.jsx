cd47c8efb49fd41eb3de9701dca62a9274bf5c1c
caio <caio96.lemos@gmail.com>
2025-02-15 19:10:56 -0300

    fix: bug nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,22 @@
-import { Link } from 'react-router-dom';
-import { useAuth } from '../services/AuthContext';
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
-    const { isAuthenticated } = useAuth();
-
-    return (
-        <>
-            {isAuthenticated ? (
-                <></>
-            ) : (
-                <nav>
-                    <ul>
-                        <li><Link to="/">Página Inicial</Link></li>
-                        <li><Link to="/busca">Busca</Link></li>
-                        <li><Link to="/cadastro-entidade">Cadastrar Entidade</Link></li>
-                        <li><Link to="/login">Entrar</Link></li>
-                    </ul>
-                </nav>
-            )}
-        </>
-    );
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Página Inicial</Link>
+        </li>
+        <li>
+          <Link to="/busca">Busca</Link>
+        </li>
+        <li>
+          <Link to="/cadastro-entidade">Cadastrar Entidade</Link>
+        </li>
+        <li>
+          <Link to="/login">Entrar</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
