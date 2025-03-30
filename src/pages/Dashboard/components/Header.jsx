8f92fb73477f1233721211414ddf6be793d62b1f
caio <caio96.lemos@gmail.com>
2025-03-30 17:18:27 -0300

    feat(Dashboard): Add campaign details navigation and product management modal

--- src/pages/Dashboard/components/Header.jsx
+++ src/pages/Dashboard/components/Header.jsx
@@ -1,20 +1,20 @@
-import PropTypes from 'prop-types';
-import '../Dashboard.css';
+import PropTypes from "prop-types";
+import "../Dashboard.css";
 
 export default function Header({ title, abrirModal }) {
-    return (
-        <header className="header--wrapper">
-            <h2>{title}</h2>
+  return (
+    <header className="header--wrapper">
+      <h2>{title}</h2>
 
-            {/* Exibe o botão apenas se o título for "Gerenciamento de Campanhas" */}
-            {title === "Gerenciamento de Campanhas" && (
-                <button onClick={abrirModal}>Nova Campanha</button>
-            )}
-        </header>
-    );
+      {/* Exibe o botão apenas se o título for "Gerenciamento de Campanhas" */}
+      {title === "Gerenciamento de Campanhas" && (
+        <button onClick={abrirModal}>Nova Campanha</button>
+      )}
+    </header>
+  );
 }
 
 Header.propTypes = {
-    title: PropTypes.string.isRequired,
-    abrirModal: PropTypes.func,
+  title: PropTypes.string.isRequired,
+  abrirModal: PropTypes.func,
 };
--- src/pages/Dashboard/views/Campanhas/idCampanha.jsx
+++ src/pages/Dashboard/views/Campanhas/idCampanha.jsx
@@ -1,12 +1,52 @@
 import { useParams } from "react-router-dom";
 import ListaProdutos from "./Components/ListaProdutos";
+import "./idCampanha.css";
+import { useEffect, useState } from "react";
+import ModalProduto from "./Components/ModalProduto";
 
 export default function IdCampanha() {
   const { idCampanha } = useParams();
 
+  const [isModalOpenProduto, setModalOpenProduto] = useState(false);
+  const [selectedCampanha, setSelectedCampanha] = useState(null);
+  const [produtos, setProdutos] = useState([]);
+  const [campanha, setCampanha] = useState(null);
+
+  const abrirModal = (e, campanhaId) => {
+    setSelectedCampanha(campanhaId);
+    setModalOpenProduto(true);
+  };
+
+  const fetchCampanha = async () => {
+
+  const fecharModal = () => {
+    setModalOpenProduto(false);
+    setSelectedCampanha(null);
+  };
+
+  const salvarProduto = (novoProduto, index) => {
+    setProdutos([...produtos, { ...novoProduto, id: index }]);
+
+    fecharModal();
+  };
+
   return (
-    <div>
+    <div className="container-produto">
+      <div className="container-buttons">
+        <button onClick={() => window.history.back()}>Voltar</button>
+        <h2>Teste</h2>
+        <button onClick={(e) => abrirModal(e, idCampanha)}>Novo produto</button>
+      </div>
       <ListaProdutos campanhaId={idCampanha} />
+
+      {isModalOpenProduto && (
+        <ModalProduto
+          isOpen={isModalOpenProduto}
+          onClose={fecharModal}
+          onSave={salvarProduto}
+          campanhaId={selectedCampanha}
+        />
+      )}
     </div>
   );
 }
